ddecd34a8518b612a0f0daedc4040a4a2f917c6c
VadimDgahm <dgahm@mail.ru>
2024-03-13 13:12:50 +0300

    add navigate /login

--- src/services/base-query-with-reauth.ts
+++ src/services/base-query-with-reauth.ts
@@ -38,7 +38,7 @@ export const baseQueryWithReauth: BaseQueryFn<
         if (refreshResult.data) {
           result = await baseQuery(args, api, extraOptions);
         } else {
-          // window.location.href = "/login";
+          window.location.href = "/login";
         }
       } finally {
         release();
