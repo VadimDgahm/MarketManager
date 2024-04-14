f41d0baee5ad98ded0917adc9fac9076fb9e0460
VadimDgahm <dgahm@mail.ru>
2024-04-14 15:35:24 +0300

    remove error

--- src/pages/briefcase/briefcase/modalCreateOrder/useCreateOrder.ts
+++ src/pages/briefcase/briefcase/modalCreateOrder/useCreateOrder.ts
@@ -33,6 +33,7 @@ export const useCreateOrder = ({ onOpenWindow, setResult }: PropsType) => {
 
         setResult(body);
         setArrProductsForClient([]);
+        setErrorAddress(false);
         setClient(undefined);
       }
       onOpenWindow(false);
