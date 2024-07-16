9e4f6e58bf1ff7ee85dac660361d7bbbb6522cf5
Maksim <madmaxxx2301@gmail.com>
2024-07-16 20:06:45 +0300

    add mobile demo-version

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,11 +1,15 @@
-import { forwardRef } from "react";
+import {forwardRef, useState} from "react";
 import s from "./header.module.scss";
 import { Typography } from "@/components/ui/typography";
+import { NavLinks} from "@/components/ui/navigate/navigate";
 
 export type HeaderInfoType = {};
 
 export const Header = forwardRef<HTMLHeadElement, HeaderInfoType>(() => {
+  const [hide, setHide] = useState<boolean>(true);
+
   return (
+    <>
     <div className={s.headerRoot}>
       <div className={s.logo}>
         <Typography variant={"body1"}>MMM</Typography>
@@ -14,9 +18,22 @@ export const Header = forwardRef<HTMLHeadElement, HeaderInfoType>(() => {
         </Typography>
       </div>
 
+      <label className={s.burger} htmlFor="burger">
+        <input type="checkbox" id="burger" onChange={() => setHide(!hide)}/>
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+
       <div>
         <span className={s.beta}>beta</span> 0.1.1
       </div>
     </div>
+    <div className={s.burgerContainer + ' ' + (hide ? s.hide : '')}>
+      <nav>
+        <NavLinks />
+      </nav>
+    </div>
+    </>
   );
 });
